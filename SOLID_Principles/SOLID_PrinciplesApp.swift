19d414e42085309cf9ce5ef26d011f050b8e4299
Nitesh Meshram <meshram.nitesh@gmail.com>
2025-08-20 18:43:42 +0530

    Initial Commit

---
+++ SOLID_Principles/SOLID_PrinciplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOLID_PrinciplesApp.swift
+//  SOLID_Principles
+//
+//  Created by NiteshMeshram on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SOLID_PrinciplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SOLID_PrinciplesTests/SOLID_PrinciplesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SOLID_PrinciplesTests.swift
+//  SOLID_PrinciplesTests
+//
+//  Created by NiteshMeshram on 8/20/25.
+//
+
+import Testing
+@testable import SOLID_Principles
+
+struct SOLID_PrinciplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
